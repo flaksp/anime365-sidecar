9064285239984f8ac1070fd5be8008e54e4518c2
Petr Flaks <petr@flaks.dev>
2026-04-05 12:17:18 +0300

    fix launching sidecar without .env file

--- cmd/sidecar/module/config.go
+++ cmd/sidecar/module/config.go
@@ -1,20 +1,41 @@
 package module
 
 import (
+	"os"
+
 	"github.com/caarlos0/env/v11"
 	"github.com/flaksp/anime365-sidecar/cmd/sidecar/config"
 	"github.com/joho/godotenv"
 )
 
-var Config = func() (*config.Env, error) {
-	err := godotenv.Load()
+func fileExists(absolutePath string) bool {
+	info, err := os.Stat(absolutePath)
 	if err != nil {
-		return nil, err
+		if os.IsNotExist(err) {
+			return false
+		}
+
+		return false
+	}
+
+	if info.IsDir() {
+		return false
+	}
+
+	return true
+}
+
+var Config = func() (*config.Env, error) {
+	if fileExists(".env") {
+		err := godotenv.Load()
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	var config config.Env
 
-	err = env.Parse(&config)
+	err := env.Parse(&config)
 	if err != nil {
 		return nil, err
 	}
